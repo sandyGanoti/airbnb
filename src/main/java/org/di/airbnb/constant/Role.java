20ac4669957aa736836abb56f929b5a3ac92d564
sandyGanoti <sandy.ganoti@gmail.com>
2020-05-01 14:41:47 +0100

    add constant Role

---
+++ src/main/java/org/di/airbnb/constant/Role.java
@@ -0,0 +1,30 @@
+package org.di.airbnb.constant;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public enum Role {
+	LANDLORD( 1 ),
+
+	TENANT( 2 ),
+
+	TENANT_AND_LANDLORD(3);
+
+	private static final Map<Integer, Role> byId = Arrays.stream( Role.values() )
+			.collect( Collectors.toMap( Role::getId, Function.identity() ) );
+	private final int id;
+
+	Role( final int id ) {
+		this.id = id;
+	}
+
+	public static Role fromId( final int id ) {
+		return byId.get( id );
+	}
+
+	public int getId() {
+		return id;
+	}
+}
