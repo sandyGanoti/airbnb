6b50d3e1f249d5c4b063c0af7422bbb7caee4793
sandyGanoti <sandy.ganoti@gmail.com>
2020-06-10 20:32:20 +0100

    disable cors

--- src/main/java/org/di/airbnb/security/WebSecurityConfig.java
+++ src/main/java/org/di/airbnb/security/WebSecurityConfig.java
@@ -60,8 +60,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure( HttpSecurity http ) throws Exception {
-		http.cors()
-				.and()
+		http
+//				.cors()
+//				.and()
 				.csrf()
 				.disable()
 				.authorizeRequests()
