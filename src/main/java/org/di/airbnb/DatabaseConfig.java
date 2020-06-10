094efe5f69c3fa8395b8993bf76c96bdb09d9510
sandyGanoti <sandy.ganoti@gmail.com>
2020-06-10 19:55:44 +0100

    --

--- src/main/java/org/di/airbnb/DatabaseConfig.java
+++ src/main/java/org/di/airbnb/DatabaseConfig.java
@@ -9,17 +9,17 @@ import org.springframework.context.annotation.Configuration;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-//@Configuration
+@Configuration
 public class DatabaseConfig {
 
-//	@Value("${spring.datasource.url}")
-//	private String dbUrl;
-//
-//	@Bean
-//	public DataSource dataSource() {
-//		HikariConfig config = new HikariConfig();
-//		config.setJdbcUrl( dbUrl );
-//		return new HikariDataSource( config );
-//	}
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl( dbUrl );
+		return new HikariDataSource( config );
+	}
 
 }
