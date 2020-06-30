4731f43fa7d41fd97a2624f490da51e70ccb8d22
sandyGanoti <sandy.ganoti@gmail.com>
2020-06-30 09:28:54 +0100

    exclude historic from search

--- src/main/java/org/di/airbnb/dao/AirbnbDaoImpl.java
+++ src/main/java/org/di/airbnb/dao/AirbnbDaoImpl.java
@@ -192,7 +192,7 @@ public class AirbnbDaoImpl {
 		}
 
 		TypedQuery<Long> query = entityManager.createQuery(
-				"select p.id from Property p where p.id in :availablePropertyIds and p.id not in (select propertyId from Booking) " +
+				"select p.id from Property p where p.id in :availablePropertyIds and p.historic=0 and p.id not in (select propertyId from Booking) " +
 						"or p.id in (select p.id from Property p join Booking b on p.id=b.propertyId  where b.propertyId not in " +
 						"(select bb.propertyId from Booking bb where (:fromDate between bb.fromDatetime and bb.toDatetime) or" +
 						"(:toDate between bb.fromDatetime and bb.toDatetime) or " +
